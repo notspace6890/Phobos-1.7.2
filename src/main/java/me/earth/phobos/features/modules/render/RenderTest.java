7c2a48b5e508e8aa286240b2e5db2b2d2195480f
TechnoPhyle <90875595+T3chn0phyl3@users.noreply.github.com>
2021-09-17 10:16:01 +1000

    Add files via upload

---
+++ src/main/java/me/earth/phobos/features/modules/render/RenderTest.java
@@ -0,0 +1,16 @@
+package me.earth.phobos.features.modules.render;
+
+import me.earth.phobos.event.events.Render3DEvent;
+import me.earth.phobos.features.modules.Module;
+
+public class RenderTest
+        extends Module {
+    public RenderTest() {
+        super("RenderTest", "RenderTest", Module.Category.RENDER, true, false, false);
+    }
+
+    @Override
+    public void onRender3D(Render3DEvent event) {
+    }
+}
+
